fabcae70af48c43454596c1529dd2da4b32117b0
476448520 <476448520@qq.com>
2022-03-09 06:20:22 +0000

    删除微博

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Status;
 use Illuminate\Http\Request;
 
 class StatusesController extends Controller
 {
     public function __construct()
     {
+        //Auth 中间件来为这两个动作添加过滤请求。
         $this->middleware('auth');
     }
 
@@ -21,6 +23,14 @@ class StatusesController extends Controller
             'content' => $request['content']
         ]);
         session()->flash('success', '发布成功！');
+        return redirect()->back();
+    }
+
+    public function destroy(Status $status)
+    {
+        $this->authorize('destroy', $status);
+        $status->delete();
+        session()->flash('success', '微博已被成功删除！');
         return redirect()->back();
     }
 }
